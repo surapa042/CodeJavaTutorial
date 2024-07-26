c59aaf11ddd30279d8431e7bba027558b1a11400
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-07-26 14:53:57 +0700

    Update StringMethods.java

--- StringMethods.java
+++ StringMethods.java
@@ -1,15 +1,22 @@
 public class StringMethods {
     public static void main(String[] args ) {
-        String message = "Java is Great Fun";
+         /* 
+         เมดธอดพื้นฐาน
+         1.toUperCase() ปรับค่าข้อความให้เป็นตัวพิมพ์ใหญ่
+         2.tolowerCase() ปรับค่าข้อความให้เป็นพิมพ์เล็ก
+         3.length() เก็บความยาวของสตริง
+         4.charAt(2) บอกให้เก็บตัวอักษรตามจำนวนพารามิตเตอร์ที่ส่งเข้าไป
+        */
+        String message = "Java is Great Fum";
         String upper = message.toUpperCase();
-        String Lower = message.toLowerCase();
-        int StringSize = message.length();
-        char letter = message.charAt(index:3);
-
+        String upper1 = message.toLowerCase();
+        int upper2 = message.length();
+        char upper3 = message.charAt(3);
         System.out.println(upper);
-        System.out.println(Lower);
-        System.out.println(StringSize);
-        System.out.println(letter);
+        System.out.println(upper1);
+        System.out.println(upper2);
+        System.out.println(upper3);
+
         
     }
 }
