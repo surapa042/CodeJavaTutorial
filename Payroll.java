432d91e647184fa7fbe1dfb38e666e5bae38db9b
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-07-26 14:53:55 +0700

    Update Payroll.java

--- Payroll.java
+++ Payroll.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 public class Payroll {
-    public class void main(String[] args) {
+    public static void main(String[] args) {
+       /*
+        1.nextByte() ชนิดข้อมูลแบบ Byte
+        2.nextDouble() ชนิดข้อมูลแบบ Double
+        3.nextFloat() ชนิดข้อมูลแบบ Float
+        4.nextInt() ชนิดข้อมูลแบบ Int
+        5.nextLine() ชนิดข้อมูลแบบ String
+        6.nextLong() ชนิดข้อมูลแบบ Long
+        7.nextShort() ชนิดข้อมูลแบบ Short
+         */
         String name;
         int hours;
         double payRate;
         double grossPay;
-        Scanner Keyboard = new Scanner (System.in);
-        System.out.print(s:"What is your name? ");
-        name = Keyboard.nextLine();
-        System.out.print(How many hours did you work this week? );
-        hours = Keyboard.nextInt();
-        System.out.print(s:"What is your hory pay rate? ");
-        payRate = Keyboard.nextDouble();
-        System.out.println("Hello"+name);
-        System.out.println("You gross pay is"+grossPay);
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("What is your name? : ");
+        name = keyboard.nextLine();
+        System.out.print("How many houer dis you work this week? : ");
+        hours = keyboard.nextInt();
+        System.out.print("what is your hourly pay rate? : ");
+        payRate = keyboard.nextDouble();
+        grossPay = hours * payRate;
+        System.out.println("Hello " + name);
+        System.out.print("Your gross pay is " + grossPay);
     }
 }
