059804d365d07994d30873e47d7fe5cd640e6ef8
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-07-26 11:14:35 +0700

    Create Payroll.java

---
+++ Payroll.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Payroll {
+    public class void main(String[] args) {
+        String name;
+        int hours;
+        double payRate;
+        double grossPay;
+        Scanner Keyboard = new Scanner (System.in);
+        System.out.print(s:"What is your name? ");
+        name = Keyboard.nextLine();
+        System.out.print(How many hours did you work this week? );
+        hours = Keyboard.nextInt();
+        System.out.print(s:"What is your hory pay rate? ");
+        payRate = Keyboard.nextDouble();
+        System.out.println("Hello"+name);
+        System.out.println("You gross pay is"+grossPay);
+    }
+}
