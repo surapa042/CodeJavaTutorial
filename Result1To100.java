ebae4b2e08bb002a9833f20fdb8c636ab520ed1c
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-08-01 12:38:01 +0700

    Create Result1To100.java

---
+++ Result1To100.java
@@ -0,0 +1,11 @@
+public class Result1To100 {
+        public static void main(String[] args) {
+            int d = 0, s = 1;
+            while (s <= 100){
+             d += s;
+                s++ ;
+        }
+        System.out.println("ผลลับ 1  ถึง 100 :"+d);
+    }
+    }
+
