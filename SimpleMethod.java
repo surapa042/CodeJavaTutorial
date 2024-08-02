5ff1648e31ed81a286d77fcdb730d53eb1691375
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-08-02 10:13:56 +0700

    Create SimpleMethod.java

---
+++ SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main method.");
+        displayMessage();
+        System.out.println("Back in the main merhod.'");
+    }
+    public static void displayMessage(){
+        System.out.println("Hello from the displayMessage method.");
+    }
+}
