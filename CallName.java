8bb60d555928a8972e8b0f8ba88bc14f2af3e600
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-08-02 10:13:47 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,13 @@
+public class CallName {
+        public static void main(String[] args) {
+            System.out.println("My Name");
+            for(int i = 1; i <= 10; i++){
+                SURAPA();
+            }
+        }
+        public static void SURAPA(){
+            System.out.println("SURAPA");
+        }
+            
+        }
+    
