f26eb0f3033a95b987e6c1088114fb1275f24ddd
surapa042 <152351738+surapa042@users.noreply.github.com>
2024-08-02 09:21:22 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,9 @@
+public class TestFor {
+    public static void main(String[] args) {
+        for(int i = 1; i <=12; i++) {
+            int total;
+            total = 2 * i;
+            System.out.println("2 x "+i+" = "+total);
+        }
+    }
+}
